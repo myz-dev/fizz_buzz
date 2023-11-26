f509dec7c5c82529dc92a3787c81ddd66fe6f31f
Mert Yildiz <mert_y52@hotmail.com>
2023-11-26 22:15:34 +0100

    test(fizz_buzz): added custom showcases.

--- lib/fizz_buzz/src/test.rs
+++ lib/fizz_buzz/src/test.rs
@@ -1,8 +1,8 @@
 use pretty_assertions::assert_eq;
 
 use crate::{
-    error::FizzBuzzError, presets::ConsecutiveTokens, FormattingOptions, Numeric, Tokenizer,
-    Traditional,
+    error::FizzBuzzError, presets::ConsecutiveTokens, FormattingOptions, Numeric, TokenCondition,
+    Tokenizer, Traditional,
 };
 
 /// Function for ergonomic test set up.
@@ -105,23 +105,176 @@ FizzBuzz"#;
     assert_eq!(&output, expected);
 }
 
-// #[test]
-// fn test_three_tokens() {
-//     let f = 2;
-//     let b = 9;
-//     let m = 5;
-//     let t = 15;
+fn create_triple_tokenizer(f: u32, b: u32, m: u32) -> (Tokenizer, FormattingOptions) {
+    let options = crate::FormattingOptions {
+        separator: Some("\n".to_string()),
+        case: None,
+    };
 
-//     let options = crate::FormattingOptions {
-//         separator: Some("\n".to_string()),
-//         case: None,
-//     };
+    let fall_back = Box::new(Numeric);
+    let fizz = Box::new(ConsecutiveTokens::new("Fizz", "+", 1, f, vec![b, m]).unwrap());
+    let buzz = Box::new(ConsecutiveTokens::new("Buzz", "+", 1, b, vec![f, m]).unwrap());
+    let mezz = Box::new(ConsecutiveTokens::new("Mezz", "+", 1, m, vec![f, b]).unwrap());
+    let fizz_buzz = Box::new(Traditional::new("FizzBuzz", 2, vec![f, b]).unwrap());
+    let mezz_buzz = Box::new(Traditional::new("MezzBuzz", 2, vec![m, b]).unwrap());
+    let fizz_mezz = Box::new(Traditional::new("FizzMezz", 2, vec![f, m]).unwrap());
+    let fizz_buzz_mezz = Box::new(Traditional::new("FizzBuzzMezz", 3, vec![f, b, m]).unwrap());
 
-//     let fall_back = Box::new(Numeric);
-//     let fizz = Box::new(ConsecutiveTokens::new("Fizz", "+", 1, f, vec![b]).unwrap());
-//     let buzz = Box::new(ConsecutiveTokens::new("Buzz", "+", 1, b, vec![f]).unwrap());
-//     let buzz = Box::new(ConsecutiveTokens::new("Mezz", "+", 1, b, vec![f]).unwrap());
-//     let fizz_buzz = Box::new(Traditional::new("FizzBuzz", 2, vec![f, b]).unwrap());
+    let tokenizer = Tokenizer::new(vec![
+        fall_back,
+        fizz,
+        buzz,
+        mezz,
+        fizz_buzz,
+        mezz_buzz,
+        fizz_mezz,
+        fizz_buzz_mezz,
+    ]);
+    (tokenizer, options)
+}
 
-//     let tokenizer = Tokenizer::new(vec![fall_back, fizz, buzz, fizz_buzz]);
-// }
+#[test]
+fn test_three_tokens() {
+    // -------------------------   f produces plusses   ------------------------- //
+
+    let f = 2;
+    let b = 9;
+    let m = 5;
+    let t = 18;
+
+    let (tokenizer, options) = create_triple_tokenizer(f, b, m);
+
+    let output = tokenizer.produce_output(t, options).unwrap();
+    let expected = r#"1
+Fizz
+3
+Fizz+
+Mezz
+Fizz
+7
+Fizz+
+Buzz
+FizzMezz
+11
+Fizz
+13
+Fizz+
+Mezz
+Fizz
+17
+FizzBuzz"#;
+    assert_eq!(&output, expected);
+
+    // -------------------------   FizzBuzzMezz   ------------------------- //
+    let f = 2;
+    let b = 3;
+    let m = 5;
+    let t = 30;
+
+    let (tokenizer, options) = create_triple_tokenizer(f, b, m);
+
+    let output = tokenizer.produce_output(t, options).unwrap();
+    let expected = r#"1
+Fizz
+Buzz
+Fizz
+Mezz
+FizzBuzz
+7
+Fizz
+Buzz
+FizzMezz
+11
+FizzBuzz
+13
+Fizz
+MezzBuzz
+Fizz
+17
+FizzBuzz
+19
+FizzMezz
+Buzz
+Fizz
+23
+FizzBuzz
+Mezz
+Fizz
+Buzz
+Fizz
+29
+FizzBuzzMezz"#;
+
+    assert_eq!(&output, expected);
+}
+
+#[test]
+fn add_custom_evaluation() {
+    struct MyFallBack;
+    impl TokenCondition for MyFallBack {
+        fn tokenize(&self, i: u32) -> String {
+            let s = i.to_string();
+            let jap = s
+                .chars()
+                .map(|c| match c {
+                    '0' => '0',
+                    '1' => '一',
+                    '2' => '二',
+                    '3' => '三',
+                    '4' => '四',
+                    '5' => '五',
+                    '6' => '六',
+                    '7' => '七',
+                    '8' => '八',
+                    '9' => '九',
+                    _ => '?',
+                })
+                .collect();
+            jap
+        }
+
+        fn condition(&self, _i: u32) -> bool {
+            true
+        }
+
+        fn get_priority(&self) -> u32 {
+            0
+        }
+    }
+
+    let f = 2;
+    let b = 10;
+    let t = 16;
+
+    let options = crate::FormattingOptions {
+        separator: Some("\n".to_string()),
+        case: None,
+    };
+
+    let fall_back = Box::new(MyFallBack);
+
+    let fizz = Box::new(ConsecutiveTokens::new("Fizz", "+", 1, f, vec![b]).unwrap());
+    let buzz = Box::new(ConsecutiveTokens::new("Buzz", "+", 1, b, vec![f]).unwrap());
+    let fizz_buzz = Box::new(Traditional::new("FizzBuzz", 2, vec![f, b]).unwrap());
+
+    let tokenizer = Tokenizer::new(vec![fall_back, fizz, buzz, fizz_buzz]);
+
+    let output = tokenizer.produce_output(t, options).unwrap();
+    let expected = r#"一
+Fizz
+三
+Fizz+
+五
+Fizz++
+七
+Fizz+++
+九
+FizzBuzz
+一一
+Fizz
+一三
+Fizz+
+一五
+Fizz++"#;
+    assert_eq!(&output, expected);
+}
